b15d1dfbb6e075114770832053d299e500c2e07e
inept <63522725+theineptdev@users.noreply.github.com>
2021-06-01 17:07:50 -0500

    26:30

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using dotnet_jokes.Models;
 
 namespace dotnet_jokes.Data
 {
@@ -12,5 +13,6 @@ namespace dotnet_jokes.Data
             : base(options)
         {
         }
+        public DbSet<dotnet_jokes.Models.Joke> Joke { get; set; }
     }
 }
